273c145b638fde89fd2716999b239585b1b0a782
Orest-Lemchyk <orestlema@gmail.com>
2025-03-12 19:49:06 +0200

    fix tests

--- lab_2/test.py
+++ lab_2/test.py
@@ -1,11 +1,13 @@
 import unittest
 from board import find_board_size
-
+ 
 class TestMinBoardSize(unittest.TestCase):
     ''' Тест функції find_board_size'''
     def test_given_cases(self):
         self.assertEqual(find_board_size(10, 2, 3), 9)
+    def test_large_case(self):
         self.assertEqual(find_board_size(2, 1000000000, 999999999), 1999999998)
+    def test_idk_how_name_it(self):
         self.assertEqual(find_board_size(4, 1, 1), 2)
 
 if __name__ == "__main__":
